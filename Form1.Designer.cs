8033b066cc0813d45203a49fec0c3e018ded73fc
Gustavo Daniel Britez Alvarenga <115171270+GustavoBritez@users.noreply.github.com>
2023-09-08 16:46:40 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Ejercicio1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.TX_1 = new System.Windows.Forms.TextBox();
+            this.TX_2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(133, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Comenzar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TX_1
+            // 
+            this.TX_1.Location = new System.Drawing.Point(94, 40);
+            this.TX_1.Name = "TX_1";
+            this.TX_1.Size = new System.Drawing.Size(151, 20);
+            this.TX_1.TabIndex = 1;
+            // 
+            // TX_2
+            // 
+            this.TX_2.Enabled = false;
+            this.TX_2.Location = new System.Drawing.Point(94, 122);
+            this.TX_2.Name = "TX_2";
+            this.TX_2.Size = new System.Drawing.Size(151, 20);
+            this.TX_2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 239);
+            this.Controls.Add(this.TX_2);
+            this.Controls.Add(this.TX_1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TX_1;
+        private System.Windows.Forms.TextBox TX_2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if( !string.IsNullOrEmpty(TX_1.Text) )
+            {
+                if ( Int32.TryParse(TX_1.Text, out int num))
+                {
+                    TX_2.Text = Recursivo(num).ToString();
+                }
+            }
+        }
+        private int Recursivo(int num)
+        {
+            int resultado = 1;
+            if (num > 1) { return num + Recursivo(num - 1); }
+
+            return resultado;
+
+        }
+    }
+}
